11c18b38466ac4f6000b97a60213026dfdd4bc8e
MaxMuecket <max.muecket@googlemail.com>
2021-04-29 12:05:46 +0200

    Add function goToPage

--- new.js
+++ new.js
@@ -1,7 +1,12 @@
 const formElement = document.querySelector(".form");
 
-function parseJSONFromLocalStorage(key) {
+function parseJSONFromLocalStorage(key, defaultValue) {
   const json = localStorage.getItem(key);
+
+  if (json === null) {
+    return defaultValue;
+  }
+
   const data = JSON.parse(json);
   return data;
 }
@@ -13,6 +18,10 @@ function appendToArray(item, array) {
 function stringifyJSONToLocalStorage(key, value) {
   const json = JSON.stringify(value);
   localStorage.setItem(key, json);
+}
+
+function goToPage(href) {
+  location.href = href;
 }
 
 formElement.onsubmit = function (event) {
@@ -39,58 +48,9 @@ formElement.onsubmit = function (event) {
     date: checkedDateInput.value,
   };
 
-  const taskList = parseJSONFromLocalStorage("taskList");
+  const taskList = parseJSONFromLocalStorage("taskList", []);
   const newTaskList = appendToArray(task, taskList);
   stringifyJSONToLocalStorage("taskList", newTaskList);
+
+  goToPage("/dashboard.html");
 };
-
-// const taskJSON = JSON.stringify(task);
-// localStorage.setItem("task", taskJSON);
-// localStorage.getItem("taskList");
-// JSON.parse(lo)
-//   const taskList = JSON.parse(task);
-// Get existing taskList (localStorage.getItem, JSON.parse)
-// const taskList - ???;
-
-// const taskArray = JSON.parse(localStorage.getItem("taskList"));
-// if (!taskArray) {
-//   localStorage.setItem("taskList");
-//   const taskArray = JSON.parse(localStorage.getItem("taskList"));
-// }
-// if (!JSON.parse(localStorage.getItem("taskList"))) {
-//   const taskArray = [""];
-// }
-
-// Append new task to exsiting taskList
-// taskArray.push(task);
-
-// Save updated taskList (localStorage.setItem, JSON.stringify)
-//   localStorage.setItem("taskList", JSON.stringify(taskArray));
-// };
-// const formElement = document.querySelector(".form");
-
-// formElement.onsubmit = function (event) {
-//   // Prevent the default form submit behaivor (sending data to a server an reloading page)
-//   event.preventDefault();
-
-//   const textInputElement = document.querySelector(".form__input");
-//   const checkedDateInput = document.querySelector(
-//     ".radio-group__input:checked"
-//   );
-
-//   if (!textInputElement.value) {
-//     alert("Text Input is empty!");
-//     return;
-//   }
-
-//   if (!checkedDateInput) {
-//     alert("Radio Button not checked!");
-//     return;
-//   }
-
-//   const task = {
-//     name: textInputElement.value,
-//     date: checkedDateInput.value,
-//   };
-
-// };
